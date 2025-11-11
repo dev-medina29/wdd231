7b84496bb0b4452616be57e2537b7f370616e817
dev-medina29 <mmbedi@byupathway.edu>
2025-11-11 18:18:48 +0100

    save

---
+++ scripts/prophets.js
@@ -0,0 +1,26 @@
+const url =
+  "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    const card = document.createElement("section");
+    const fullName = document.createElement("h2");
+    const portrait = document.createElement("img");
+    fullName.innerHTML = `${prophet.name} ${prophet.lastname}`;
+    portrait.setAttribute("Alt", `${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "l00");
+    portrait.setAttribute("height", "l00");
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+    cards.appendChild(card);
+  });
+};
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets);
+  displayProphets(data.prophets);
+}
+getProphetData();
