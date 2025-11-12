2e5057c1dc6534e2a9125d595921b6c74bbc283f
dev-medina29 <mmbedi@byupathway.edu>
2025-11-12 11:07:38 +0100

    save

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,25 +1,32 @@
-const MyData="";
+const MyData = "https://dev-medina29.github.io/wdd231/chamber/data/member.json";
 const cards = document.querySelector("#cards");
 const displayMembers = (members) => {
   members.forEach((member) => {
     const card = document.createElement("section");
     const fullName = document.createElement("h2");
     const portrait = document.createElement("img");
-    fullName.innerHTML = `${member.name} ${member.lastname}`;
-    portrait.setAttribute("Alt", `${member.name} ${member.lastname}`);
+    const detail = document.createElement("a");
+    const descrip = document.createElement("p");
+    fullName.innerHTML = `${member.name}`;
+    portrait.setAttribute("Alt", member.name);
     portrait.setAttribute("src", member.image);
     portrait.setAttribute("loading", "lazy");
     portrait.setAttribute("width", "l00");
     portrait.setAttribute("height", "l00");
-    card.appendChild(fullName);
+    detail.setAttribute("href", member.website);
+    detail.innerHTML = "Details";
+    descrip.innerHTML = member.description;
     card.appendChild(portrait);
+    card.appendChild(fullName);
+    card.appendChild(descrip);
+    card.appendChild(detail);
     cards.appendChild(card);
   });
 };
 async function getMembersData() {
   const response = await fetch(MyData);
   const data = await response.json();
-  // console.table(data.prophets);
-  displayMembers(data.prophets);
+  // console.table(data.members);
+  displayMembers(data.members);
 }
 getMembersData();
