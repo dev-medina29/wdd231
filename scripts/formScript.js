7ca245e3e932d1fec4267e882ce644a2e4b36f24
dev-medina29 <mmbedi@byupathway.edu>
2025-12-06 19:40:41 +0100

    save

---
+++ scripts/formScript.js
@@ -0,0 +1,11 @@
+// select the form and input fields
+const form = document.getElementById("best-form");
+const passwordInput = document.getElementById("password");
+const repeatPasswordInput = document.getElementById("repeat-password");
+const emailInput = document.getElementById("email");
+const nameInput = document.getElementById("name");
+const firstnameInput = document.getElementById("firstname");
+
+form.addEventListener("submit", ()=> {
+
+});
